afeff77c63ab1d730aeaba89381da5fbc9a314ac
Kyle Cutler <kyle1320@gmail.com>
2015-06-19 16:15:10 -0400

    minor changes

--- js/site.js
+++ js/site.js
@@ -5,12 +5,21 @@
 		header.innerHTML = '\
 			<h1>My Projects</h1> \
 			<ul id="menu"> \
+				<a href="/index.html"><li>Home</li></a> \
 				<a href="/projects/spirograph/spiro.html"><li>Spirograph</li></a> \
 				<a href="/projects/graphs/graphs.html"><li>Graphs</li></a> \
 				<a href="/projects/particles/particles.html"><li>Particles</li></a> \
 				<li>More to come...</li> \
 			</ul> \
 		';
-		document.body.insertBefore(header, document.body.firstChild);
+		
+		var content = document.createElement('div');
+		content.id = 'content';
+		while (document.body.firstChild) {
+			content.appendChild(document.body.firstChild);
+		}
+		
+		document.body.appendChild(header);
+		document.body.appendChild(content);
 	});
 }());
