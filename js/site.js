6f5387246762c9c9066ae6ba6a6aece4a7723fb5
Kyle Cutler <kyle1320@gmail.com>
2015-06-19 16:07:37 -0400

    attempting to remove iframe

--- js/site.js
+++ js/site.js
@@ -1,3 +1,16 @@
-window.onload = function() {
-	document.getElementById("menu").getElementsByTagName("a")[0].click();
-}
+(function() {
+	window.addEventListener('load', function() {
+		var header = document.createElement('div');
+		header.id = 'header';
+		header.innerHTML = '\
+			<h1>My Projects</h1> \
+			<ul id="menu"> \
+				<a href="/projects/spirograph/spiro.html"><li>Spirograph</li></a> \
+				<a href="/projects/graphs/graphs.html"><li>Graphs</li></a> \
+				<a href="/projects/particles/particles.html"><li>Particles</li></a> \
+				<li>More to come...</li> \
+			</ul> \
+		';
+		document.body.insertBefore(header, document.body.firstChild);
+	});
+}());
