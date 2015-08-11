4ae90f776b5abd92a322c476c0ca62bb476d8768
Kyle Cutler <kyle1320@gmail.com>
2015-08-11 16:28:27 -0400

    added minified files

--- projects/js/cloth/cloth.js
+++ projects/js/cloth/cloth.js
@@ -3,14 +3,14 @@ function Cloth(canvas) {
 	this.context = canvas.getContext("2d");
 	this.points = [];
 	this.paused = true;
-	
+
 	scaleCanvas(this.canvas, this.context);
-	
+
 	var width = canvas.clientWidth;
 	var height = canvas.clientHeight;
-	
+
 	var cl = this;
-	
+
 	var gravity = 400.0;
 	var correction = 1.0;
 	var dampen = 0.01;
@@ -18,16 +18,16 @@ function Cloth(canvas) {
 	var interactiondist = 20;
 	var interactionforce = 2000;
 	var padding = 50;
-	
+
 	var mouse = null;
 	var mousemov = null;
-	
+
 	var pauseBtn = document.getElementById("pause");
 	var resetBtn = document.getElementById("reset");
-	
+
 	pauseBtn.addEventListener("click", function() {cl.setPaused(!cl.paused);});
 	resetBtn.addEventListener("click", function() {cl.reset();});
-	
+
 	function Point(x, y, pinned, px, py) {
 		this.x = x;
 		this.y = y;
@@ -35,35 +35,35 @@ function Cloth(canvas) {
 		this.ly = y;
 		this.ax = 0;
 		this.ay = 0;
-		
+
 		this.pinned = pinned || false;
 		this.px = px === undefined ? x : px;
 		this.py = py === undefined ? y : py;
-		
+
 		this.links = [];
-		
+
 		this.update = function(time) {
 			this.interaction();
 			this.applyForce(0, gravity);
-			
+
 			var mx = (this.x - this.lx) * (1.0 - dampen);
 			var my = (this.y - this.ly) * (1.0 - dampen);
-			
+
 			this.lx = this.x;
 			this.ly = this.y;
-			
+
 			this.x += mx + 0.5 * this.ax * time * time;
 			this.y += my + 0.5 * this.ay * time * time;
-			
+
 			this.ax = 0;
 			this.ay = 0;
 		};
-		
+
 		this.applyForce = function(fx, fy) {
 			this.ax += fx;
 			this.ay += fy;
 		};
-		
+
 		this.interaction = function() {
 			if (mouse && mousemov) {
 				var dx = mouse.x - this.x;
@@ -75,7 +75,7 @@ function Cloth(canvas) {
 				}
 			}
 		};
-		
+
 		this.constraints = function() {
 			for (var i=0; i < this.links.length; i++) {
 				this.links[i].solve();
@@ -102,29 +102,29 @@ function Cloth(canvas) {
 				this.y = this.py;
 			}
 		};
-		
+
 		this.attachTo = function(p, restingDist, strength) {
 			this.links[this.links.length] = new Link(this, p, restingDist, strength);
 		};
 	}
-	
+
 	function Link(a, b, restingDist, strength) {
 		if (restingDist === undefined) {
 			var dx = a.x - b.x;
 			var dy = a.y - b.y;
 			restingDist = Math.sqrt(dx*dx + dy*dy);
 		}
-		
+
 		if (strength === undefined) {
 			strength = 1;
 		}
-		
+
 		this.a = a;
 		this.b = b;
 		this.restingDist = restingDist;
 		this.tearDist = restingDist * (strength + 1);
 		this.broken = false;
-		
+
 		this.solve = function() {
 			var dx = this.a.x - this.b.x;
 			var dy = this.a.y - this.b.y;
@@ -134,22 +134,22 @@ function Cloth(canvas) {
 
 			//pull = (d - restingDist) / (tearDist - restingDist);
 
-			if (d > this.tearDist) 
+			if (d > this.tearDist)
 				this.broken = true;
 			if (d < restingDist)
 				return;
 
 			var tx = dx * correction/2.0 * difference;
 			var ty = dy * correction/2.0 * difference;
-			
+
 			a.x += tx;
 			a.y += ty;
-			
+
 			b.x -= tx;
 			b.y -= ty;
 		};
 	}
-	
+
 	this.setPaused = function(paused) {
         if (paused && !this.paused) {
             clearInterval(this.runInterval);
@@ -158,11 +158,11 @@ function Cloth(canvas) {
                 cl.update(0.015);
             }, 15);
         }
-        
+
         this.paused = paused;
 		pauseBtn.innerHTML = paused ? "Resume" : "Pause";
     };
-	
+
 	this.getRelativeCoord = function(evt) {
         var rect = canvas.getBoundingClientRect();
         return {
@@ -170,10 +170,10 @@ function Cloth(canvas) {
             y: evt.clientY - rect.top
         };
     };
-	
+
 	this.draw = function() {
 		this.context.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
-		
+
 		for (var i=0; i < this.points.length; i++) {
 			for (var j=0; j < this.points[i].links.length; j++) {
 				this.context.beginPath();
@@ -183,14 +183,14 @@ function Cloth(canvas) {
 				this.context.strokeStyle = "#000000";
 				this.context.stroke();
 			}
-			
+
 			this.context.beginPath();
 			this.context.arc(this.points[i].x, this.points[i].y, 2, 0, 2 * Math.PI);
 			this.context.closePath();
 			this.context.fillStyle = "#000000";
 			this.context.fill();
 		}
-		
+
 		if (mouse) {
 			this.context.globalAlpha = 0.2;
 			this.context.beginPath();
@@ -201,29 +201,29 @@ function Cloth(canvas) {
 			this.context.globalAlpha = 1.0;
 		}
 	};
-	
+
 	this.update = function(time) {
 		for (var i=0; i < iterations; i++) {
 			for (var j=0; j < this.points.length; j++) {
 				this.points[j].constraints();
 			}
 		}
-		
+
 		this.points.forEach(function (p) {p.update(time);});
 		this.draw();
 	};
-	
+
 	this.reset = function() {
 		this.points = [];
 		this.initializePoints();
 	};
-	
+
 	this.initializePoints = function() {
 		var width = 20;
 		var height = 20;
 		var stepx = 8;
 		var stepy = 8;
-		
+
 		var i=0;
 		for (var y=0; y < height; y++) {
 			for (var x=0; x < width; x++) {
@@ -236,9 +236,9 @@ function Cloth(canvas) {
 			}
 		}
 	};
-	
+
 	this.initializePoints();
-	
+
 	canvas.addEventListener("mousedown", function(e) {
 		var newmouse = cl.getRelativeCoord(e);
 		if (mouse) mousemov = {x: newmouse.x - mouse.x, y: newmouse.y - mouse.y};
@@ -275,7 +275,7 @@ function scaleCanvas(canvas, context) {
 var cloth;
 window.onload = function() {
 	var canvas = document.getElementById("drawCanvas");
-	
+
 	cloth = new Cloth(canvas);
 	cloth.setPaused(false);
 };
