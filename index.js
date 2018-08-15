9a406b74afe569225f8777fb1cd08a99da39dab0
Kyle Cutler <kyle1320@gmail.com>
2018-08-15 19:22:25 -0400

    Add 404 page

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const Express = require('express');
 const path = require('path');
 
-module.exports = Express.static(path.join(__dirname, 'public'));
+module.exports = {
+  files: Express.static(path.join(__dirname, 'public')),
+  handle404: function(req, res) {
+    res.status(404).sendFile(path.join(__dirname, 'public/404.html'));
+  }
+};
