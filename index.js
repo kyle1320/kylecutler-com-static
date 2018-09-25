a548fd2a3ab8b1e905f7bc8dc0683363c5f7fd61
Kyle Cutler <kyle1320@gmail.com>
2018-09-24 22:13:18 -0400

    Refactor module exports - no longer depend on Express.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const Express = require('express');
 const path = require('path');
 
 module.exports = {
-  files: Express.static(path.join(__dirname, 'public')),
-  handle404: function(req, res) {
-    res.status(404).sendFile(path.join(__dirname, 'public/404.html'));
-  }
+  rootDir: path.join(__dirname, 'public'),
+  page404: path.join(__dirname, 'public/404.html')
 };
