123c622254fde5038cafd2e24a3c38030c5bc511
Kyle Cutler <kyle1320@gmail.com>
2018-10-02 16:41:25 -0400

    Add 3-input varietes of AND, OR, and XOR gates

--- src/scripts/standalone/_circuits/model/Circuits.js
+++ src/scripts/standalone/_circuits/model/Circuits.js
@@ -80,4 +80,49 @@ module.exports = {
       "path": "M0,0 2,0 2,2 0,2 Z"
     }
   },
+  "3-Input And": {
+    "size": { "width": 2, "height": 2 },
+    "pins": [
+      { "x": 0, "y": 0.3, "ignoreInput": false },
+      { "x": 0, "y": 1,   "ignoreInput": false },
+      { "x": 0, "y": 1.7, "ignoreInput": false },
+      { "x": 2, "y": 1,   "ignoreInput": true }
+    ],
+    "rules": [
+      { "type": "output", "target": 3, "value": "$0 $1 $2 & &" }
+    ],
+    "style": {
+      "path": "M0,0 1,0 A1,1 0 1,1 1,2 L0,2 Z"
+    }
+  },
+  "3-Input Or": {
+    "size": { "width": 2, "height": 2 },
+    "pins": [
+      { "x": 0.2, "y": 0.3, "ignoreInput": false },
+      { "x": 0.2, "y": 1,   "ignoreInput": false },
+      { "x": 0.2, "y": 1.7, "ignoreInput": false },
+      { "x": 2,   "y": 1,   "ignoreInput": true }
+    ],
+    "rules": [
+      { "type": "output", "target": 3, "value": "$0 $1 $2 | |" }
+    ],
+    "style": {
+      "path": "M0,0 C0,0 1.3,0 2,1 2,1 1.3,2 0,2 0,2 1,1 0,0 Z"
+    }
+  },
+  "3-Input Xor": {
+    "size": { "width": 2, "height": 2 },
+    "pins": [
+      { "x": 0.2, "y": 0.3, "ignoreInput": false },
+      { "x": 0.4, "y": 1,   "ignoreInput": false },
+      { "x": 0.2, "y": 1.7, "ignoreInput": false },
+      { "x": 2,   "y": 1,   "ignoreInput": true }
+    ],
+    "rules": [
+      { "type": "output", "target": 3, "value": "$0 $1 $2 ^ ^" }
+    ],
+    "style": {
+      "path": "M.2,0 C.2,0 1.4,0 2,1 2,1 1.4,2 .2,2 .2,2 1.2,1 .2,0 Z M0,0 C0,0 1,1 0,2 0,2 1,1 0,0 Z"
+    }
+  },
 };
