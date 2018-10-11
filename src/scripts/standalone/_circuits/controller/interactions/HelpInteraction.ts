750324a9ad92448ca110554c34cca8e257082b7b
Kyle Cutler <kyle1320@gmail.com>
2018-10-11 14:54:21 -0400

    Update help text to reflect actionbar changes

--- src/scripts/standalone/_circuits/controller/interactions/HelpInteraction.ts
+++ src/scripts/standalone/_circuits/controller/interactions/HelpInteraction.ts
@@ -25,37 +25,44 @@ export default class HelpInteraction extends Interaction {
 <h2>How do I use this app?</h2>
 <p>The center design area holds the elements in the current workspace. It consists of an unbounded grid where elements can be added, removed, moved, and modified as desired.</p>
 <p>There are a few different types of elements: nodes, connections, and circuits. Nodes are a single point of connection without any logic. Circuits are, well, circuits, and can be anything from a simple AND gate to something much more complicated. Connections are the lines between nodes, and will propagate any signal from one node to the other.</p>
-<p>Available actions are grouped into various tools, each of which allows the user to interact with objects in different ways.</p>
-<p>A tool can be selected by clicking on its icon in the toolbar at the very top of the page. Information relevant to the selected tool will be display in the info bar just below the toolbar at the top of the page.</p>
-<p>What follows is a list of the available tools and their uses:</p>
+<p>The Action Bar at the top of the screen shows the various available tools and actions, grouped into sections. A brief description of each element in the action bar can be seen by hovering over the item.</p>
+<p>There are three types of elements within the action bar: buttons, toggles, and tools.</p>
+<p>Buttons perform an instantaneous action, like deleting an element or showing this help dialog.</p>
+<p>Toggles control whether or not an option is enabled or disabled.</p>
+<p>Tools are similar to toggles, but only one can be selected at a time. The selected tool dictates what actions can be performed when interacting with the grid.</p>
+<p>What follows is a list of the action bar sections and the options available in each.</p>
 
-<h3>The Pointer Tool</h3>
-<p>The pointer tool allows the user to select and interact with objects on the grid.</p>
-<p>A circuit or connection can be selected by clicking on it. Once selected, the info bar will show options for the selected element like deleting or rotating it.</p>
+<h3>Select</h3>
+<p>The select section consists of the Select tool, as well as various buttons for interacting with selections.</p>
+<h4>The Select Tool</h4>
+<p>The select tool allows the user to select and interact with objects on the grid, and is the default tool used if no other tool is selected.</p>
+<p>When this tool is selected, A circuit or connection can be selected by clicking on it. Once selected, relevant action buttons will be enabled to allow actions like deleting or rotating the selected element(s).</p>
 <p>If a node is clicked, it will be toggled on or off.</p>
 <p>Multiple elements can be selected by clicking and dragging over multiple elements.</p>
 <p>The Ctrl key can be held down while selecting elements in order to modify the current selection.</p>
 <p>All elements in the workspace can be selected quickly by using the Ctrl+A keyboard shortcut.</p>
 
-<h3>The Drag Tool</h3>
+<h3>Drag</h3>
+<p>The drag section consists of the Drag tool, and the "Snap to Grid" toggle.</p>
+<h4>The Drag Tool</h4>
 <p>The drag tool allows the user to move elements or the grid.</p>
-<p>Simply click and drag an element in order to move it around the grid.</p>
-<p>If multiple elements are selected, they will all be moved together.</p>
-<p>By default, elements can be placed anywhere on the grid. In order to ensure a clean workspace, elements can be "snapped" to the grid. To enable this, either selecting the "snap to grid" option in the info bar, or hold the Shift key while dragging an element.</p>
+<p>When this tool is selected, simply click and drag an element in order to move it around the grid.</p>
+<p>If multiple elements are selected, when one if dragged they will all be moved together.</p>
+<h4>Snap to Grid</h4>
+<p>When this toggle is enabled, instead of allowing elements to be positioned anywhere, elements will be "snapped" to the gridlines.</p>
 
-<h3>The Add Tool</h3>
-<p>The add tool allows the user to create new nodes, circuits, and connections.</p>
-<p>To create a new node or circuit, simply select the desired element from the info bar and click anywhere in the design area to add the element to the workspace.</p>
-<p>To create connections, click and drag from any node. When the mouse is released, if the drag ended on an existing node, that node will be connected to the starting node. Otherwise a new node will be created and connected.</p>
+<h3>Create</h3>
+<p>The create section contains several creation-related tools, each one of which corresponds with an element that can be created.</p>
+<p>To create a new node or circuit, simply select the desired tool and click anywhere in the design area to add the element to the workspace.</p>
+<p>To create connections, select any tool in this section, then click and drag from any node. When the mouse is released, if the drag ended on an existing node, that node will be connected to the starting node. Otherwise a new node will be created and connected.</p>
 
-<h3>The Zoom In / Out Tools</h3>
-<p>The zool tools allow the user to zoom in and out of the grid. Simply select one of the tools and click anywhere on the grid.</p>
-<p>Scrolling the mouse (or pinching with two fingers on mobile) will also allow zooming at any time, even if another tool is selected.</p>
+<h3>Zoom</h3>
+<p>The zoom section contains buttons for zooming in and out. Simply click one of the tools and the grid will zoom in or out.</p>
+<p>In addition to these buttons, scrolling the mouse over the grid (or pinching with two fingers on mobile) will also allow zooming at any time.</p>
 
-<h3>The Import / Export Tools</h3>
-<p>The import / export tools allow the user to save and load their creations.</p>
-<p>When using the export tool, any currently selected elements will be added to the data to be exported. If no elements are selected, then all elements in the workspace will be included.</p>
-<p>When exporting, save the generated text somewhere safe so it can be imported later.</p>
+<h3>Data</h3>
+<p>The data section provides buttons for saving and loading data. It contains buttons for exporting and importing data. Clicking either of the buttons will open a dialog window.</p>
+<p>When exporting, any currently selected elements will be added to the data to be exported. If no elements are selected, then all elements in the workspace will be included. Save the generated text somewhere safe so it can be imported later.</p>
 <p>When importing, simply paste the previously exported text and click "Import" to load the previously saved data.</p>
 
 <h3>Keyboard Shortcuts</h3>
