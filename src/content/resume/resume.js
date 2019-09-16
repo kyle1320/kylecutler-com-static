af5e2d52041e4e515e46336846d45a57aca786e3
Kyle Cutler <kyle1320@gmail.com>
2019-09-16 11:03:21 -0400

    Update résumé, minor style changes

--- src/content/resume/resume.js
+++ src/content/resume/resume.js
@@ -6,6 +6,8 @@ window.addEventListener('load', function () {
   var printBtn = document.getElementById('print-btn');
   var showMoreButtons = document.querySelectorAll('.show-more-btn');
 
+  document.getElementById('resume').className = 'resume show';
+
   printBtn.addEventListener('click', () => window.print());
 
   for (var i = 0; i < showMoreButtons.length; i++) {
